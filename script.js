262eb4d0b75d2bf97e8419f60396f17feb727ebe
ArissaHoriye <aline_arissa@hotmail.com>
2024-10-05 16:12:14 -0300

    html-meta, start js

---
+++ script.js
@@ -0,0 +1,27 @@
+//var startButton = document.getElementById("btn-start");
+//var stopButton = document.getElementById("btn-stop");
+/*
+let counter = 0;
+let timeout;
+let timerOn = 0;
+
+function timedCount() {
+    document.getElementById("myTime").value = counter;
+    counter ++;
+    timeout = setTimeout(timedCount,1000);
+}
+
+function start() {
+    if (! timerOn) {
+        timerOn = 1;
+        timedCount();
+    }
+}
+
+function stop() {
+    clearTimeout(timeout);
+    timemerOn = 0;
+}
+*/
+
+
